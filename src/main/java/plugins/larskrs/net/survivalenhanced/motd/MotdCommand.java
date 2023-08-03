8664cca4058f4fc2839c4a6a9237bbfd65a94269
= <larskrs@smage.org>
2023-08-03 16:13:03 +0200

    Added an InventoryTool for base64 encryption to inventories.

--- src/main/java/plugins/larskrs/net/survivalenhanced/motd/MotdCommand.java
+++ src/main/java/plugins/larskrs/net/survivalenhanced/motd/MotdCommand.java
@@ -1,9 +1,15 @@
 package plugins.larskrs.net.survivalenhanced.motd;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import plugins.larskrs.net.survivalenhanced.general.Command;
+import plugins.larskrs.net.survivalenhanced.tools.InventoryTool;
+import plugins.larskrs.net.survivalenhanced.tools.Messanger;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MotdCommand extends Command {
@@ -22,7 +28,7 @@ public class MotdCommand extends Command {
     public void execute(CommandSender sender, String[] args) {
         Player player = (Player) sender;
 
-        MotdMenu menu = new MotdMenu(args[00]);
+        MotdMenu menu = new MotdMenu(args[0]);
         menu.OpenGUI(player);
     }
 
